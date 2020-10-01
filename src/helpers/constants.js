39906896e206010a7d5d5bb7148160b8536d61a5
Andres Fernando <andres.fernando08@gmail.com>
2020-10-01 16:03:05 -0500

    updated tokens for api

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://us.api.blizzard.com/data/wow';
 export const API_TOKEN_URL = 'https://us.battle.net/oauth/token';
 
-export const API_CLIENT_ID = '';
-export const API_CLIENT_SECRET = '';
+export const API_CLIENT_ID = process.env.REACT_APP_API_CLIENT_ID;
+export const API_CLIENT_SECRET = process.env.REACT_APP_API_CLIENT_SECRET;
